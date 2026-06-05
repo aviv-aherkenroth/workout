c3b521c5dd89fda15354fa4b36d734f22bc0b70b
Adrian Herkenroth <144799732+aviv-aherkenroth@users.noreply.github.com>
2026-06-05 13:28:48 +0200

    disable sw.js for now

--- sw.js
+++ sw.js
@@ -1,39 +1,7 @@
-const CACHE = 'workout-tracker-v2';
-const ASSETS = [
-  '/workout/',
-  '/workout/index.html',
-  '/workout/manifest.json',
-  '/workout/icon-192.png',
-  '/workout/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      Promise.allSettled(ASSETS.map(url => c.add(url).catch(() => {})))
-    ).then(() => self.skipWaiting())
-  );
-});
-
+// disabled
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => caches.match('/workout/index.html'));
-    })
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
 });
